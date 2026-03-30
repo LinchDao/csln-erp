3f22e4240fa2141d1a0748dab52c80f62651fe04
linch <linchuhanio@gmail.com>
2026-03-30 18:38:17 +0800

    feat：引入mybatisplus分页

---
+++ src/main/java/com/lin/csln/config/MyBatisPlusConfig.java
@@ -0,0 +1,27 @@
+package com.lin.csln.config;
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description:
+ * @Author: linch
+ */
+
+
+@Configuration
+public class MyBatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //分页插件
+        PaginationInnerInterceptor paginationInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        interceptor.addInnerInterceptor(paginationInterceptor);
+        return interceptor;
+    }
+}
